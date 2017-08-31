efa6cb6141e9977db893c126cae2027369acd51e
wmm1002 <1059214609@qq.com>
2017-08-31 22:55:37 +0800

    DEVELOP:kaifa

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -83,7 +83,18 @@
         this.setFullScreen(true)
       },
       enter(el,done){
-
+        const {x,y,scale}=this._getPosAndScale();
+        let animation={
+          0:{
+            transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+          },
+          60%:{
+            transform: `translate3d(0,0,0) scale(1.1)`
+          },
+          100%:{
+            transform: `translate3d(0,0,0) scale(1)`
+          }
+        }
       },
       afterEnter(){
 
@@ -93,6 +104,22 @@
       },
       afterLeave(){
 
+      },
+      _getPosAndScale(){
+        const targetWidth=40
+        const paddingLeft=40
+        const paddigBottom=30
+        const paddingTop=80
+        const width=window.innerWidth*0.8
+        const scale=targetWidth/width
+        const x=-(window.innerWidth/2-paddingLeft)
+        const y=window.innerHeight-paddingTop-paddingBottom
+
+        return{
+          x,
+          y,
+          scale
+        }
       },
       ...mapMutations({
         setFullScreen: 'SET_FULL_SCREEN'
