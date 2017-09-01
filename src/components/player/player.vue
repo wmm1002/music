44ef9a8a1dc5a8293dd381867c4765cf65c5960d
ALLY.WANG <ally.wang@ruis-ims.cn>
2017-09-01 16:50:57 +0800

    DEVELOP:播放器开发

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -19,8 +19,8 @@
         </div>
         <div class="middle">
           <div class="middle-l">
-            <div class="cd-wrapper">
-              <div class="cd">
+            <div class="cd-wrapper" ref="cdWrapper">
+              <div class="cd" :class="cdCls">
                 <img class="image" :src="currentSong.image" alt="">
               </div>
             </div>
@@ -35,7 +35,7 @@
               <i class="icon-prev"></i>
             </div>
             <div class="icon i-center">
-              <i class="icon-play"></i>
+              <i @click="togglePlaying" :class="playIcon"></i>
             </div>
             <div class="icon i-right">
               <i class="icon-next"></i>
@@ -51,28 +51,45 @@
     <transition name="mini">
   		<div class="mini-player" v-show="!fullScreen" @click="open">
   			<div class="icon">
-          <img width="40" height="40" :src="currentSong.image" alt="">   
+          <img :class="cdCls" width="40" height="40" :src="currentSong.image" alt="">   
         </div>
         <div class="text">
           <h2 class="name" v-html="currentSong.name"></h2>
           <p class="desc" v-html="currentSong.singer"></p>
         </div>
-        <div class="control"></div>
+        <div class="control">
+          <i @click.stop="togglePlaying" :class="miniIcon"></i>
+        </div>
         <div class="control">
           <i class="icon-playlist"></i>
         </div>
   		</div>
     </transition>
+    <audio ref="audio" :src="currentSong.url"></audio>
 	</div>
 </template>
 <script>
 	import {mapGetters,mapMutations} from 'vuex'
+  import animations from 'create-keyframe-animation'
+  import {prefixStyle} from 'common/js/dom'
+
+  const transform=prefixStyle('transform')
 	export default{
 		computed: {
+      cdCls(){
+        return this.playing?'play':'play pause'
+      },
+      playIcon(){
+        return this.playing?'icon-pause':'icon-play'
+      },
+      miniIcon(){
+        return this.playing?'icon-pause-mini':'icon-play-mini'
+      },
 			...mapGetters([
 				'fullScreen',
 				'playlist',
-        'currentSong'
+        'currentSong',
+        'playing'
 			])
 		},
     methods:{
@@ -83,32 +100,51 @@
         this.setFullScreen(true)
       },
       enter(el,done){
-        const {x,y,scale}=this._getPosAndScale();
+        const {x,y,scale}=this._getPosAndScale()
         let animation={
           0:{
             transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
           },
-          60%:{
+          60:{
             transform: `translate3d(0,0,0) scale(1.1)`
           },
-          100%:{
+          100:{
             transform: `translate3d(0,0,0) scale(1)`
           }
         }
+
+        animations.registerAnimation({
+          name: 'move',
+          animation,
+          presets:{
+            duration: 400,
+            easing: 'linear'
+          }
+        })
+
+        animations.runAnimation(this.$refs.cdWrapper,'move',done)
       },
       afterEnter(){
-
+        animations.unregisterAnimation('move')
+        this.$refs.cdWrapper.style.animation=''
       },
       leave(el,done){
-
+        this.$refs.cdWrapper.style.transition='all 0.4s'
+        const {x,y,scale}=this._getPosAndScale()
+        this.$refs.cdWrapper.style[transform]=`translate3d(${x}px,${y}px,0) scale(${scale})`
+        this.$refs.cdWrapper.addEventListener('transitionend',done)
       },
       afterLeave(){
-
+        this.$refs.cdWrapper.style.transition=''
+        this.$refs.cdWrapper.style[transform]=''
+      },
+      togglePlaying(){
+        this.setPlayingState(!this.playing)
       },
       _getPosAndScale(){
         const targetWidth=40
         const paddingLeft=40
-        const paddigBottom=30
+        const paddingBottom=30
         const paddingTop=80
         const width=window.innerWidth*0.8
         const scale=targetWidth/width
@@ -122,8 +158,22 @@
         }
       },
       ...mapMutations({
-        setFullScreen: 'SET_FULL_SCREEN'
+        setFullScreen: 'SET_FULL_SCREEN',
+        setPlayingState: 'SET_PLAYING_STATE'
       })
+    },
+    watch: {
+      currentSong(){
+        this.$nextTick(()=>{
+          this.$refs.audio.play()
+        })
+      },
+      playing(newPlaying){
+        const audio=this.$refs.audio
+        this.$nextTick(()=>{
+          newPlaying?audio.play():audio.pause()
+        })
+      }
     }
 	}
 </script>
